f8e4ea6d7a5fb7f64ac1dbb732f3bffd7e73a506
Nard Theo <theo.nard18@gmail.com>
2024-02-04 17:47:52 +0100

    feat: New idea

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env /usr/bin/python3
 
-from fastapi import FastAPI, WebSocket
-import os, time
+import os, time, sys, signal
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-import uvicorn
 from dotenv import load_dotenv
+import requests
+
 import logging
+from colorlog import ColoredFormatter
+
+LOG_LEVEL = logging.DEBUG
+LOGFORMAT = "  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
+logging.root.setLevel(LOG_LEVEL)
+formatter = ColoredFormatter(LOGFORMAT)
+stream = logging.StreamHandler()
+stream.setLevel(LOG_LEVEL)
+stream.setFormatter(formatter)
+log = logging.getLogger('pythonConfig')
+log.setLevel(LOG_LEVEL)
+log.addHandler(stream)
 
 load_dotenv()
 
@@ -18,45 +30,67 @@ WRITE_CLIENT = InfluxDBClient(
 
 WRITE_API = WRITE_CLIENT.write_api(write_options=SYNCHRONOUS)
 
-app = FastAPI()
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+def shelly_collector():
+    gen1 = os.environ["SHELLY_GEN1"].split(",")
+    gen2 = os.environ["SHELLY_GEN2"].split(",")
 
-@app.get("/")
-async def get():
-    return "Hello World"
+    for shelly in gen1 + gen2:
+        log.info(f'Shelly {shelly} added to monitoring')
 
-@app.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        component = {"name": ""}
-        while True:
-            data = await websocket.receive_json()
-            log.info(data)
-            if data["method"] == "NotifyFullStatus":
-                if "wifi" in data["params"]:
-                    component["name"] = data["params"]["wifi"]["sta_ip"]
-                    log.info("%s: Connected" % component["name"])
-            if data["method"] == "NotifyStatus":
-                if "switch:0" in data["params"]:
-                    if "apower" in data["params"]["switch:0"]:
-                        log.info("%s: %s"% (component["name"], data["params"]["switch:0"]["apower"]))
-                        point = (
-                            Point("switch")
-                            .field("apower", data["params"]["switch:0"]["apower"])
-                            .tag("name", component["name"])
-                            .time(time.time_ns(), WritePrecision.NS)
-                        )
-                        WRITE_API.write(
-                            bucket=os.environ["INFLUXDB_BUCKET"],
-                            org=os.environ["INFLUXDB_ORG"],
-                            record=point,
-                        )
-    except Exception as e:
-        log.error("%s: Disconnected (%s)" % (component["name"], e))
-        return
+    log.info("Starting Shelly collector")
+    while True:
+        try:
+            for shelly in gen1:
+                request = requests.get(f"http://{shelly}/meter/0", timeout=2)
+                data = request.json()
+                power = data["power"]
+                total = data["total"]
+                WRITE_API.write(
+                    os.environ["INFLUXDB_BUCKET"],
+                    os.environ["INFLUXDB_ORG"],
+                    [
+                        Point("switch")
+                        .tag("shelly", shelly)
+                        .field("power", power)
+                        .field("total", total)
+                        .time(time.time_ns(), WritePrecision.NS)
+                    ],
+                )
+                log.info(f"Shelly {shelly} - Power: {power}, Total: {total}")
+            for shelly in gen2:
+                request = requests.get(f"http://{shelly}/rpc/Switch.GetStatus?id=0", timeout=2)
+                data = request.json()
+                power = data["apower"]
+                total = data["aenergy"]["total"]
+                temp = data["temperature"]["tC"]
+                volt = data["voltage"]
+                current = data["current"]
+                WRITE_API.write(
+                    os.environ["INFLUXDB_BUCKET"],
+                    os.environ["INFLUXDB_ORG"],
+                    [
+                        Point("switch")
+                        .tag("shelly", shelly)
+                        .field("power", power)
+                        .field("total", int(total))
+                        .field("temp", temp)
+                        .field("volt", volt)
+                        .field("current", current)
+                        .time(time.time_ns(), WritePrecision.NS)
+                    ],
+                )
+                log.info(f"Shelly {shelly} - Power: {power}, Total: {total}, Temp: {temp}, Volt: {volt}, Current: {current}")
+        except Exception as e:
+            log.error(e)
+        time.sleep(5)
+
+
+#Handle ctrl c
+def signal_handler(sig, frame):
+    log.warning('You pressed Ctrl+C!')
+    sys.exit(0)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    signal.signal(signal.SIGINT, signal_handler)
+    shelly_collector()
