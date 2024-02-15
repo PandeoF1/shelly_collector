a572d420f2efcbdb97d6c4531f16115a8d4395c3
Nard Theo <theo.nard@it-development.com>
2024-02-15 09:04:12 +0100

    fix: patch datetime..

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ def shelly_collector():
     }
     while True:
         try:
-            if (datetime.now() - tempo["last_check"]).seconds >= 60 * tempo["check_delay"]:
-                tempo["last_check"] = datetime.now(pytz.timezone(os.environ["TIMEZONE"]))
+            if (pytz.timezone("Europe/Paris").localize(datetime.now()) - tempo["last_check"]).seconds >= 60 * tempo["check_delay"]:
+                tempo["last_check"] = pytz.timezone("Europe/Paris").localize(datetime.now())
                 try:
                     request = requests.get(f"https://www.api-couleur-tempo.fr/api/jourTempo/today", timeout=2)
                     data = request.json()
@@ -55,7 +55,7 @@ def shelly_collector():
                     tempo["last_check"] = datetime.now()
                 except Exception as e:
                     log.error(e)
-            if datetime.now(pytz.timezone(os.environ["TIMEZONE"])).hour <= 6 or datetime.now(pytz.timezone(os.environ["TIMEZONE"])).hour >= 22:
+            if pytz.timezone("Europe/Paris").localize(datetime.now()).hour <= 6 or pytz.timezone("Europe/Paris").localize(datetime.now()).hour >= 22:
                 time_ = "HC"
             else:
                 time_ = "HP"
